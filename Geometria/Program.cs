45ad37a83cbdabb60be3bb1da597630ea348b063
tessleo99 <t.napoletano9@gmail.com>
2023-04-17 15:59:08 +0200

    stampa valori in console

--- Geometria/Program.cs
+++ Geometria/Program.cs
@@ -1,2 +1,13 @@
 ﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+
+using Geometria;
+
+Console.Write("Inserisci la base del rettangolo in cm: ");
+float baseRettangoloUser = float.Parse(Console.ReadLine());
+
+Console.Write("Inserisci l'altezza del rettangolo in cm: ");
+float altezzaRettangoloUser = float.Parse(Console.ReadLine());
+
+Rettangolo rettangolo1 = new Rettangolo(baseRettangoloUser, altezzaRettangoloUser);
+
+rettangolo1.StampaRettangolo();
--- Geometria/Rettangolo.cs
+++ Geometria/Rettangolo.cs
@@ -6,7 +6,45 @@ using System.Threading.Tasks;
 
 namespace Geometria
 {
-    internal class Rettangoli
+    public class Rettangolo
     {
+        //ATTRIBUTI
+        public float baseRettangolo;
+        public float altezzaRettangolo;
+
+        //STATI
+
+        //COSTRUTTORE
+        public Rettangolo(float baseRettangolo, float altezzaRettangolo)
+        {
+            this.baseRettangolo = baseRettangolo;
+            this.altezzaRettangolo = altezzaRettangolo;
+        }
+
+        //METODI
+       
+        //METODO PER IL CALCOLO DEL PERIMETRO
+        public float CalcolaPerimetroRettangolo()
+        {
+            float perimetroRettangolo = (baseRettangolo + altezzaRettangolo) * 2;
+            return perimetroRettangolo;
+        }
+
+        //METODO PER IL CALCOLO DELL'AREA
+        public float CalcolaAreaRettangolo()
+        {
+            float areaRettangolo = (baseRettangolo * altezzaRettangolo);
+            return areaRettangolo;
+        }
+
+        //METODO PER LA STAMPA DEI VALORI DEL RETTANGOLO
+        public void StampaRettangolo()
+        {
+            Console.WriteLine("--Rettangolo--");
+            Console.WriteLine("Base: " + baseRettangolo + "cm");
+            Console.WriteLine("Altezza: " + altezzaRettangolo + "cm");
+            Console.WriteLine("Perimetro: " + CalcolaPerimetroRettangolo() + "cm");
+            Console.WriteLine("Area:" + CalcolaAreaRettangolo() + "cm^2");
+        }
     }
 }
